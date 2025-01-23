3fe341448a8314502750a5f1a7d24e3421865480
Damian Thomas <dimitrisonic@gmail.com>
2025-01-23 16:08:24 -0500

    Add @astrojs/mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 export const site = "https://fromthesuperhighway.com";
 export const siteEmail = "newsletter@fromthesuperhighway.com";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), icon(), react()],
-	site,
-	output: "static",
-	redirects: {
-		"/not-found": "/404",
-	},
-	// vite: { ssr: { noExternal: false } },
-	// adapter: cloudflare({ imageService: "cloudflare" }),
+    integrations: [tailwind(), icon(), react(), mdx()],
+    site,
+    output: "static",
+    redirects: {
+        "/not-found": "/404",
+    },
+    // vite: { ssr: { noExternal: false } },
+    // adapter: cloudflare({ imageService: "cloudflare" }),
 });
