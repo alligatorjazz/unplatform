3ff95496cb10e0314b9bc72b5e2d01e931227500
Damian Thomas <dimitrisonic@gmail.com>
2025-01-23 12:38:19 -0500

    Remove old React components;

--- src/components/ReactPrompt.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-
-export function ReactPrompt({ children, className }: { children: ReactNode, className?: string }) {
-	return <div className={["flex", className].join(" ")}>
-		<span className={"mr-2 text-inherit"}>{">:"}</span>
-		{children}
-	</div>
-}
