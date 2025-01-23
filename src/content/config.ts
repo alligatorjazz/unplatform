79bf7a221ee0fcb621cebd4c10f65504030da964
Damian Thomas <dimitrisonic@gmail.com>
2025-01-23 11:03:46 -0500

    Add initial types for content collections; Add robots.txt; Remove extraneous FTS files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,48 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
-import { IssueSchema } from 'fts-types';
+import { defineCollection, z } from 'astro:content';
+
+const RecommendationTypeSchema = z.enum([
+	"newspaper",
+	"magazine",
+	"blog",
+	"software",
+	"platform",
+	"organization",
+	"other"
+]);
+
+const OperatingSystemSchema = z.enum(["ios", "android", "windows", "mac", "linux", "web"]);
+const PricingSchema = z.enum(["free", "freemium", "paid"]);
 
 // 2. Define a `type` and `schema` for each collection
-const issuesCollection = defineCollection({
-	type: "content", // v2.5.0 and later
-	schema: IssueSchema,
+const RecommendationSchema = z.object({
+	url: z.string(),
+	title: z.string(),
+	type: RecommendationTypeSchema,
+	os: OperatingSystemSchema.array(),
+	pricing: PricingSchema,
+	dateAdded: z.date(),
+	lastUpdated: z.date()
 });
 
+const recommendationCollection = defineCollection({
+	type: "content", // v2.5.0 and later
+	schema: RecommendationSchema,
+});
+
+const LibraryItemSchema = z.object({
+	url: z.string(),
+	title: z.string(),
+	author: z.string(),
+	tags: z.string().array()
+})
+
+const libraryCollection = defineCollection({
+	type: "content", // v2.5.0 and later
+	schema: LibraryItemSchema,
+});
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-	'issues': issuesCollection,
+	'recommendationy': recommendationCollection,
+	'library': libraryCollection
 };
