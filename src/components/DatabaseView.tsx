4ceee48d158d2384d2e65d52ead7b5588a776355
Damian Thomas <dimitrisonic@gmail.com>
2025-01-25 02:48:45 -0500

    Add resetFilters() to DatabaseView.tsx

--- src/components/DatabaseView.tsx
+++ src/components/DatabaseView.tsx
@@ -1,5 +1,5 @@
 import type { CollectionEntry } from "astro:content";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState, type FormEvent } from "react";
 import { FiMonitor } from "react-icons/fi";
 import { toTitleCase } from "../lib/dom";
 import { RecommendationCategories, type LiteracyLevel, type RecommendationCategory } from "../types";
@@ -20,7 +20,7 @@ export interface DatabaseViewProps {
 }
 
 export default function DatabaseView({ title, entries, defaultOptions, hideOptions }: DatabaseViewProps) {
-	const { register, watch } = useForm<DatabaseViewOptions>({
+	const { register, watch, reset } = useForm<DatabaseViewOptions>({
 		defaultValues: {
 			freeOnly: false,
 			category: "all",
@@ -30,6 +30,13 @@ export default function DatabaseView({ title, entries, defaultOptions, hideOptio
 	});
 
 	const filters = watch();
+	const resetFilters = useCallback(() => {
+		reset({
+			freeOnly: false,
+			category: "all",
+			maxComplexity: "4",
+		});
+	}, []);
 
 	const filteredEntries = useMemo(() => {
 		let result = entries;
@@ -52,12 +59,12 @@ export default function DatabaseView({ title, entries, defaultOptions, hideOptio
 
 	return (
 		<section className="w-full rounded-md border border-textColor">
-			<div className="p-4 border-b border-textColor w-full flex flex-col">
-				<h1 className="font-bold text-center">{title}</h1>
-
+			<div className="p-4 border-b border-textColor w-full flex items-center gap-4">
+				<h1 className="font-bold">{title}</h1>
+				{filteredEntries.length < entries.length && <p className="italic text-xs">{entries.length - filteredEntries.length} items hidden due to filters.</p>}
 			</div>
 			<div className="max-h-96 min-h-96 overflow-y-scroll flex flex-col-reverse md:flex-row">
-				<ul className="border-textColor md:border-r">
+				<ul className="border-textColor md:border-r h-max">
 					{filteredEntries.map(({ data }) => (
 						<li key={data.url}>
 							<a target="_blank" className="p-4 block w-full h-full border-b border-textColor no-underline cursor-pointer hover:brightness-150 hover:backdrop-brightness-125 will-change-auto" href={data.url}>
@@ -93,7 +100,7 @@ export default function DatabaseView({ title, entries, defaultOptions, hideOptio
 						</li>
 					))}
 				</ul>
-				<form className="text-xs h-full border-b border-textColor md:border-b-0 md:sticky top-1/3 whitespace-nowrap p-4 flex flex-wrap justify-center gap-2 min-w-48 flex-1">
+				<form className="text-xs h-full border-b border-textColor md:border-b-0 md:sticky top-1/3 whitespace-nowrap p-4 flex flex-wrap items-center justify-center gap-2 min-w-48 flex-1 max-h-32 md:max-h-full">
 					<div className="flex items-center gap-1">
 						<input type="checkbox" {...register("freeOnly")} />
 						<label htmlFor="freeOnly">Free Options Only</label>
@@ -114,6 +121,7 @@ export default function DatabaseView({ title, entries, defaultOptions, hideOptio
 							))}
 						</select>
 					</div>
+					<button type="button" onPointerDown={resetFilters} className="p-4 py-2 bg-accentColor text-bgColor transition-all hover:brightness-125 h-min w-min whitespace-nowrap">{">: "}Reset Filters</button>
 				</form>
 			</div>
 		</section>
