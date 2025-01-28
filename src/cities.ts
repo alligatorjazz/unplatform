f6b84d311628ed7a73b20a2aa9cb7e8132d89f66
Damian Thomas <dimitrisonic@gmail.com>
2025-01-27 21:26:04 -0500

    Populate cities.ts

---
+++ src/cities.ts
@@ -0,0 +1,12 @@
+export const Cities = [
+	"All",
+	"Digital First",
+	"New York, NY, USA",
+	"Chicago, IL, USA",
+	"Los Angeles, CA, USA",
+	"Worcester, MA, USA",
+	"Brisbane, QLD, AU",
+	"Denver, CO, USA",
+	"Omaha, NE, USA",
+	"Orlando, FL, USA"
+] as const;
