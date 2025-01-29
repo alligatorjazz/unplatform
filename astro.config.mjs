fa50bc48e3a15e5c38bc7d60a9f5e0fe2c04021c
Damian Thomas <dimitrisonic@gmail.com>
2025-01-29 10:09:45 -0500

    Add RSS support (lol)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import markdownIntegration from "@astropub/md";
 
 import mdx from "@astrojs/mdx";
 
@@ -10,12 +11,12 @@ export const siteEmail = "newsletter@fromthesuperhighway.com";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon(), react(), mdx()],
-    site,
-    output: "static",
-    redirects: {
-        "/not-found": "/404",
-    },
-    // vite: { ssr: { noExternal: false } },
-    // adapter: cloudflare({ imageService: "cloudflare" }),
+	integrations: [tailwind(), icon(), react(), mdx(), markdownIntegration()],
+	site,
+	output: "static",
+	redirects: {
+		"/not-found": "/404",
+	},
+	// vite: { ssr: { noExternal: false } },
+	// adapter: cloudflare({ imageService: "cloudflare" }),
 });
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,32 @@
+import rss from '@astrojs/rss';
+import { markdown } from '@astropub/md';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import dayjs from 'dayjs';
+import { site } from '../../astro.config.mjs';
+import { toTitleCase } from '../lib/dom';
+
+export async function GET(ctx: APIContext) {
+	// TODO: add library support
+	const entries = await getCollection("recommendations");
+	return rss({
+		title: "Unplatform",
+		description: "The definitive guide for escaping social media (and joining the indie web.)",
+		site,
+		trailingSlash: false,
+		items: await Promise.all(entries.map(async (entry) => {
+			return {
+				title: entry.data.title,
+				pubDate: dayjs(entry.data.dateAdded).toDate(),
+				description: entry.body,
+				link: entry.data.url,
+				categories: (entry.data.category ?? [""]),
+				content: `<p>${Array.isArray(entry.data.city) ? entry.data.city.join(" + ") : entry.data.city} | ${entry.data.pricing.map(price => toTitleCase(price)).join(" or ")} | ${entry.data.category.map(category => toTitleCase(category)).join(", ")} | Supports: ${entry.data.os.map(os => os === "ios" ? "iOS" : toTitleCase(os)).join(", ")}</p>`
+					+ await markdown(entry.body),
+				customData: `
+					<markdown>${entry.body}</markdown>
+				`
+			}
+		}))
+	});
+}
